e1cd597899bf47c4b81229801ce1fdcf70ec9ba1
Harsh Ahuja <harshahuja@Harshs-MacBook-Air.local>
2025-01-15 08:40:23 +0530

    package-json-changes

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/api/users', (req, res) => {
+  res.send('Hello World!')
+})  
+
+app.get('/api/users/:id', (req, res) => {
+  res.send(`Hello World! User ID: ${req.params.id}`)
+})
+
+app.get('/api/users/:id/posts', (req, res) => {
+  res.send(`Hello World! User ID: ${req.params.id}, Posts`)
+})  
+app.get('/api/users/:id/posts/:postId', (req, res) => {
+  res.send(`Hello World! User ID: ${req.params.id}, Post ID: ${req.params.postId}`)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
